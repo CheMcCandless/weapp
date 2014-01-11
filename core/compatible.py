e9bb6186afc39d496efbd9f776856d64cf76d451
“CodeFalling <code.falling@gmail.com>
2014-01-11 21:45:55 +0800

    make it wrong again

--- core/compatible.py
+++ core/compatible.py
@@ -4,7 +4,7 @@ import settings
 
 def getmc():
     severSet = settings.read(settings.MOD_SETTING_SERVER)
-    if severSet[settings.MOD_SETTING_SERVER]:
+    if severSet[settings.SET_SERVER_ONSAE]:
         import pylibmc
         mc = pylibmc.Client()
         return mc
