5d7470be40b2611b3280e472fcaba972245627c6
“CodeFalling <code.falling@gmail.com>
2014-01-11 17:41:56 +0800

    memcached on sae

--- core/compatible.py
+++ core/compatible.py
@@ -5,7 +5,9 @@ import settings
 def getmc():
     severSet = settings.read(settings.MOD_SETTING_SEVER)
     if severSet[settings.SET_SEVER_ONSAE]:
-        pass
+        import pylibmc
+        mc = pylibmc.Client()
+        return mc
     else:
         import memcache
         mc = memcache.Client([severSet[settings.SET_SEVER_MEMCACHE]])
